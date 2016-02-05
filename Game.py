4470554a4910e036d2dfa840fccaa2df90e071ba
abcp4 <navi1921@gmail.com>
2016-02-05 17:56:39 -0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,86 +1,21 @@
-import math
 import random
-#each value card has a 1:13 chance of being selected (we don't care about suits for blackjack)
-#cards (value): Ace (1), 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack (10), Queen (10), King (10)
 
-def randomCard():
-    card = random.randint(1,13)
-    if card > 10:
-       card = 10
-    return card
 
-#A hand is just a tuple e.g. (14, False), a total card value of 14 without a useable ace
-#accepts a hand, if the Ace can be an 11 without busting the hand, it's useable
-def useable_ace(hand):
-    val, ace = hand
-    return ((ace) and ((val + 10) <= 21))
+print "Reshuffled list : ",  pieces
 
-def totalValue(hand):
-    val, ace = hand
-    if (useable_ace(hand)):
-        return (val + 10)
-    else:
-        return val
+def take_pieces(pieces):
+    for i in pieces:
+        
+    
 
-def add_card(hand, card):
-    val, ace = hand
-    if (card == 1):
-        ace = True
-    return (val + card, ace)
-#The first is first dealt a single card, this method finishes off his hand
-def eval_dealer(dealer_hand):
-    while (totalValue(dealer_hand) < 17):
-        dealer_hand = add_card(dealer_hand, randomCard())
-    return dealer_hand
-
-#state: (player total, useable_ace), (dealer total, useable ace), game status; e.g. ((15, True), (9, False), 1)
-#stay or hit => dec == 0 or 1
-def play(state, dec):
-    #evaluate
-    player_hand = state[0] #val, useable ace
-    dealer_hand = state[1]
-    if dec == 0: #action = stay
-        #evaluate game; dealer plays
-        dealer_hand = eval_dealer(dealer_hand)
-
-        player_tot = totalValue(player_hand)
-        dealer_tot = totalValue(dealer_hand)
-        status = 1
-        if (dealer_tot > 21):
-            status = 2 #player wins
-        elif (dealer_tot == player_tot):
-            status = 3 #draw
-        elif (dealer_tot < player_tot):
-            status = 2 #player wins
-        elif (dealer_tot > player_tot):
-            status = 4 #player loses
-
-    elif dec == 1: #action = hit
-        #if hit, add new card to player's hand
-        player_hand = add_card(player_hand, randomCard())
-        d_hand = eval_dealer(dealer_hand)
-        player_tot = totalValue(player_hand)
-        status = 1
-        if (player_tot == 21):
-            if (totalValue(d_hand) == 21):
-                status = 3 #draw
-            else:
-                status = 2 #player wins!
-        elif (player_tot > 21):
-            status = 4 #player loses
-        elif (player_tot < 21):
-            #game still in progress
-            status = 1
-    state = (player_hand, dealer_hand, status)
-
-    return state
 
 #start a game of blackjack, returns a random initial state
 def initGame():
     status = 1 #1=in progress; 2=player won; 3=draw; 4 = dealer won/player loses
-    player_hand = add_card((0, False), randomCard())
-    player_hand = add_card(player_hand, randomCard())
-    dealer_hand = add_card((0, False), randomCard())
+    pieces = [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(2,2),(2,3),(2,4),(2,5),(2,6),(3,3),(3,4),(3,5),(3,6),(4,4),(4,5),(4,6),(5,5),(5,6),(6,6)]
+    random.shuffle(pieces)
+    player_hand = take_pieces(pieces)
+    dealer_hand = take_pieces(pieces)
     #evaluate if player wins from first hand
     if totalValue(player_hand) == 21:
         if totalValue(dealer_hand) != 21:
@@ -90,3 +25,4 @@ def initGame():
 
     state = (player_hand, dealer_hand, status)
     return state
+
