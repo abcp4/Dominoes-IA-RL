caf29e98afe68d12625eb00da9a586028545ef1d
abcp4 <navi1921@gmail.com>
2016-02-05 22:15:57 -0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,28 +1,35 @@
+import math
 import random
+from random import shuffle
 
 
-print "Reshuffled list : ",  pieces
-
-def take_pieces(pieces):
-    for i in pieces:
-        
+def take_initial_pieces(hand,pieces):
+    n = 0
+    while(n<7):
+        hand.append(pieces[n])
+        #print pieces[n]
+        n= n+1
+    del pieces[0:7]
+    return hand
+    
+    
+def play(state action):
     
 
-
-#start a game of blackjack, returns a random initial state
+#Start a game of dominoes, shuffling pieces and giving 7 to each player
 def initGame():
     status = 1 #1=in progress; 2=player won; 3=draw; 4 = dealer won/player loses
+    
     pieces = [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(2,2),(2,3),(2,4),(2,5),(2,6),(3,3),(3,4),(3,5),(3,6),(4,4),(4,5),(4,6),(5,5),(5,6),(6,6)]
     random.shuffle(pieces)
-    player_hand = take_pieces(pieces)
-    dealer_hand = take_pieces(pieces)
-    #evaluate if player wins from first hand
-    if totalValue(player_hand) == 21:
-        if totalValue(dealer_hand) != 21:
-            status = 2 #player wins after first deal!
-        else:
-            status = 3 #draw
-
-    state = (player_hand, dealer_hand, status)
+    player_hand = []
+    dealer_hand = []
+    player_hand = take_initial_pieces(player_hand,pieces)
+    dealer_hand = take_initial_pieces(dealer_hand,pieces)
+    field_pieces = []
+    #retorna estado visivel ao jogador:
+    #sua mão,peças no campo(0 inicialmente), estado do jogo 
+    #seria aqui o vetor de caracteristicas? 
+    #tudo que for util para representar o estado?
+    state = (player_hand, field_pieces,status)
     return state
-
