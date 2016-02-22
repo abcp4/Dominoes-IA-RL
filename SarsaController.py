15df6380b7bf9ac1662845f478b2e6d2059f8792
abcp4 <navi1921@gmail.com>
2016-02-22 15:58:00 -0300

    Update SarsaController.py

--- SarsaController.py
+++ SarsaController.py
@@ -1,66 +1,75 @@
+import random
+import numpy
+import scipy
 
 
+class Features:
+    numFeatures = 60
+    #valida caracteristicas de acordo com o estado submetido e as retorna
+    def featureValues(state):
+        result =  np.zeros(numFeatures)
+        print state
+
+def rand_winner (S_from, beta):
+    sum = 0.0
+    p_i = 0.0
+    rnd = numpy.random.random()
+    d_r = len (S_from)
+    sel = 0
+
+    for i in range (d_r):
+        sum += numpy.exp (beta * S_from[i])
+
+    S_target = numpy.zeros (d_r)
+
+    for i in range (d_r):
+        p_i += numpy.exp (beta * S_from[i]) / sum
+
+        if  p_i > rnd:
+            sel = i
+            rnd = 1.1 # out of reach, so the next will not be turned ON
+
+    return sel
+
+
+
+
+size_a, size_b = 16, 16
+size_map = size_a * size_b
+size_mot = 4
+featureWeights = numpy.random.uniform (0.0, 0.0, (size_mot, size_map))
+
+beta = 50.0
+features = Features()
+
+for iter in range (10000):
+    
+    state = dominoes.startGame() 
+    act = policyAct (state)                         # take action by a e-greedy policy
+    featureVals = features.featuresValue(state)
+    val = numpy.dot (featureWeights, featureVals)                     # value before action
+    r = 0
+    duration = 0
+    termination = 0
+    while termination == 0:
+
+        duration += 1
+        world.act(act)                                  # do selected action
+        r = world.reward()                              # read reward
+        I_tic = world.sensor()                          # read new state
 
         
+        act_tic = rand_winner (h, beta)                 # choose next action
 
-class SARSA():
-    """ State-Action-Reward-State-Action (SARSA) algorithm.
-    In batchMode, the algorithm goes through all the samples in the
-    history and performs an update on each of them. if batchMode is
-    False, only the last data sample is considered. The user himself
-    has to make sure to keep the dataset consistent with the agent's
-    history."""
+        act_vec = numpy.zeros (size_mot)
+        act_vec[act] = 1.0
 
-    offPolicy = False
-    batchMode = True
+        val_tic = numpy.dot (w_mot[act_tic], I_tic)     # value after action
+        target = r+ 0.9 * val_tic                     # gamma = 0.9
+        delta = target - val                            # prediction error
 
-    def __init__(self, alpha=0.5, gamma=0.99):
+        w_mot += 0.5 * delta * numpy.outer (act_vec, I) #pra cada peso no array de caracteristicas aplica o gradient descent update
 
-        self.alpha = alpha
-        self.gamma = gamma
-
-        self.laststate = None
-        self.lastaction = None
-
-    def qvalue(featureVals):
-        tot = 0
-        it1 = np.nditer(featureVals, flags=['f_index'])
-        it2 = np.nditer(featureWeigths, flags=['f_index'])
-        while not it1.finished:
-            tot += it1[0]*it2[0]
-            it1.iternext()
-            it2.iternext()
+        val = val_tic
+        act = act_tic
         
-    return tot
-    def learn(self):
-        if self.batchMode:
-            samples = self.dataset
-        else:
-            samples = [[self.dataset.getSample()]]
-
-        for seq in samples:
-            # information from the previous episode (sequence)
-            # should not influence the training on this episode
-            self.laststate = None
-            self.lastaction = None
-            self.lastreward = None
-            for state, action, reward in seq:
-
-                state = int(state)
-                action = int(action)
-
-                # first learning call has no last state: skip
-                if self.laststate == None:
-                    self.lastaction = action
-                    self.laststate = state
-                    self.lastreward = reward
-                    continue
-
-                qvalue = self.module.getValue(self.laststate, self.lastaction)
-                qnext = self.module.getValue(state, action)
-                self.module.updateValue(self.laststate, self.lastaction, qvalue + self.alpha * (self.lastreward + self.gamma * qnext - qvalue))
-
-                # move state to oldstate
-                self.laststate = state
-                self.lastaction = action
-                self.lastreward = reward
