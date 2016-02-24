f4b6d5d0ae71291db233812f3dc43035cf422f9e
abcp4 <navi1921@gmail.com>
2016-02-24 00:52:06 -0300

    Update SarsaController.py

--- SarsaController.py
+++ SarsaController.py
@@ -11,29 +11,11 @@ class Features:
         
         return result
 
-def rand_winner (S_from, beta):
-    sum = 0.0
-    p_i = 0.0
-    rnd = numpy.random.random()
-    d_r = len (S_from)
-    sel = 0
+def policyAct(state):
+    #constroi acoes possiveis a partir do estado
+    #escolhe a partir dos valores na q-value com de e-greedy
 
-    for i in range (d_r):
-        sum += numpy.exp (beta * S_from[i])
-
-    S_target = numpy.zeros (d_r)
-
-    for i in range (d_r):
-        p_i += numpy.exp (beta * S_from[i]) / sum
-
-        if  p_i > rnd:
-            sel = i
-            rnd = 1.1 # out of reach, so the next will not be turned ON
-
-    return sel
-
-
-
+    return action
 
 num_features = 60
 featureWeights = np.zeros(numFeatures)
