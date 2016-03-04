49a0661559cd87e14a3780174eeb9ede33377917
abcp4 <navi1921@gmail.com>
2016-03-03 22:29:36 -0300

    Update SarsaController.py

--- SarsaController.py
+++ SarsaController.py
@@ -1,5 +1,5 @@
 import random
-import numpy
+import numpy as np
 import scipy
 
 
@@ -11,87 +11,89 @@ featureWeights = np.zeros(numFeatures)
 beta = 50.0
 features = Features()
 
-
 class Features:
-    numFeatures = 60
-    
-    def featureValues(state,action):
-        """"
+    def featureValues(self,state,action):
+        numFeatures = 60
+        hand = state[1]
+        l_end = state[4]
+        r_end = state[5]
+        """
         Características a partir de informações imperfeitas.
             Somente do que é visível a um jogador comum:
                 Campo, peças de suas mãos, quantidade de peças do oponente
-        """"
+        """
         #valida caracteristicas de acordo com o estado submetido e as retorna
         result =  np.zeros(numFeatures)
-        
-        result[0] = hasOneDouble(state)
-        result[1] = blocksMe(state,action)#impossibilita qualquer peça de ser jogada
-        result[2] = NumActions(state,action,1)#retorna true se o num de ações disp após essa ação for 1
-        result[3] = NumActions(state,action,2)
-        result[4] = NumActionsSince(state,action,3)#possibilita 3 ou mais peças de serem jogáveis
+        if(action[1]=="left"):
+        #substitui uma das pontas pela nova ação
+            l_end = action[0]
+        else:
+            r_end = action[0]
+        value =hand.pop(pos)
+    
+        result[0] = self.hasOneDouble(hand,action,l_end,r_end)
+        result[1] = self.blocksMe(hand,action,l_end,r_end)#impossibilita qualquer peça de ser jogada
+        hand.insert(pos,value)
+        """
+        result[2] = self.NumActions(hand,action,l_end,r_end,1)#retorna true se o num de ações disp após essa ação for 1
+        result[3] = self.NumActions(hand,action,l_end,r_end,2)
+        result[4] = self.NumActionsSince(hand,action,l_end,r_end,3)#possibilita 3 ou mais peças de serem jogáveis
         for i in range(7):
             #quais peças duplas temos na mão, de 0:0 a 6:6
-            result[i] = hasDouble(i)
-        
-        
-        """"
+            result[i] = self.hasDouble(i,hand,action,l_end,r_end)
+        """
+        """
         Características a partir de informações perfeitas.
         Sabe-se da mão do oponente, de toda e qualquer informação necessária
         para se tomar uma decisão
-        """"
+        """
         
         
         
         return result
     
-    def hasOneDouble(state):
-        hand = state[1]
+    def hasOneDouble(self,hand,action,l_end,r_end):
         for piece in hand:
             if(piece[0] == piece[1]):
                 return True
         return False
     
-    def blocksMe(state,action):
-        l_end = state[4]
-        r_end = state[5]
-        hand = state[1]
-        substituteForNewPiece(action,hand,l_end,r_end)
-        
+    def hasDouble(self,number,hand,action):
         for piece in hand:
-            if(isPlayable(piece,l_end,r_end)):
+            if((piece[0] == piece[1]) and piece[0] == number):
+                return True
+        return False
+    
+    def blocksMe(self,hand,action,l_end,r_end):      
+        for piece in hand:
+            if(self.isPlayable(piece,l_end,r_end)):
                 #se encontra alguma peça valida na mao
                 return False # então ainda não estou bloqueado
   
         return True #nao encontrou
         
-    def NumActions(state,action,num):
-        hand = state[1]
-        substituteForNewPiece(action,hand,l_end,r_end)
+    def NumActions(self,hand,action,l_end,r_end,num):
         count = 0
         for piece in hand:
-            if(isPlayable(piece,l_end,r_end)):
+            if(self.isPlayable(piece,l_end,r_end)):
                 count = count+1
             if(count>num):#se tem mais que o numero de peças designado
                 return False
         return True #se tem exatamente aquele numero
         
-    def NumActionsSince(state,action,num):
-        
+    def NumActionsSince(self,hand,action,l_end,r_end,num):
+        count = 0
+        for piece in hand:
+            if(self.isPlayable(piece,l_end,r_end)):
+                count = count+1
+            if(count>=num):#se tem o mesmo ou mais que o numero de peças designado
+                return True
+        return False #se tem menos             
                
-               
-    def isPlayable(piece,l_end,r_end):#se dado a peça e as duas pontas, ela é jogavel
+    def isPlayable(self,piece,l_end,r_end):#se dado a peça e as duas pontas, ela é jogavel
         if(piece[0]==l_end or piece[0]==r_end or piece[1]==l_end or piece[1]==r_end):
                return True
         return False
-    
-    def substituteForNewPiece(action,hand,l_end,r_end):#funcao util para ver campo apos ação
-        if(action[1]=="left"): #substitui uma das pontas pela nova ação
-            l_end = action[0]
-        elif:
-            r_end = action[0]
-        pos = action[2]
-        hand.pop(pos)#remove a ação(peça) da mão
-        
     
                
 def qValue(state,act):
